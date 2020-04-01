44aba321bfac3c4de58c48b6a0c7bba4f8a1adf2
Sonal <sonali.patwe@gmail.com>
2020-04-01 20:32:30 +0530

    Loops

---
+++ src/Loops.java
@@ -0,0 +1,23 @@
+
+public class Loops {
+
+	public static void main(String args[]) {
+		System.out.println("Using for loop");
+		for(int i=0; i<=10; i++){
+			System.out.println("i = "+i);
+		}
+		System.out.println("Using While loop");
+		int j=0;
+		while( j<=10){
+			System.out.println("j = "+j);
+			j++;
+		}
+		System.out.println("Using Do While loop");
+		int k=0;
+		do{
+			System.out.println("k = "+k);
+			k++;
+		}while(k<=10);
+		
+	}
+}
