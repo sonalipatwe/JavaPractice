710da1177a99505b0d04a97706b9533c26248c64
Sonali <sonali.patwe@gmail.com>
2020-04-01 20:22:51 +0530

    Scanner

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String args[]){
+			System.out.println("Enter your name:");
+			Scanner name = new Scanner( System.in );
+			System.out.println("Hello "+name.next());
+		}
+}
